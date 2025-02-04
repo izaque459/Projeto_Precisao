55aed37765b73755fc045f3250378cd600d02820
Isaac_Santos <isaacdsantos@hotmail.com>
2025-02-04 18:16:10 -0300

    Modificado arquivo precisao.c

--- precisao.c
+++ precisao.c
@@ -4,3 +4,20 @@ float  a ;
 double b ;
 int    i ;
 
+
+int main()
+{
+	a = 0.0f;
+	b = 0.0;
+	i = 0;
+
+	
+	while(i<1000000)
+	{
+		a += .000001;
+		b += .000001;
+		i += 1;
+	}
+	printf("\n O valor de a eh %f",a);
+	printf("\n O valor de b eh %f",b);
+}
