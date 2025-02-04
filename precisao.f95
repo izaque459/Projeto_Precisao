db1a7e760a40f021f5c4af4a48042a2577d5aa92
Isaac_Santos <isaacdsantos@hotmail.com>
2025-02-04 17:57:27 -0300

    Modificado arquivo precisao

--- precisao.f95
+++ precisao.f95
@@ -2,7 +2,19 @@ PROGRAM precisao
     IMPLICIT NONE
     REAL :: a
     DOUBLE PRECISION :: b
+    INTEGER :: i
     
+    a = 0.0
+    b = 0.0D0
+    i = 0
     
+    DO WHILE(i.lt.1000000)
+        a = a + 0.000001
+        b = b + 0.000001D0
+	    i = i + 1
+    END DO
 
+	WRITE(*,*) "O valor de a eh",a
+	WRITE(*,*) "O valor de b eh",b
+	
 END PROGRAM precisao
